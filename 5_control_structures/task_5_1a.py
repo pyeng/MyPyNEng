461a3b4d3e9715f2a968f4e3d695f3fc4b1ca904
pyeng <pyeng@users.noreply.github.com>
2017-02-14 18:05:06 +0200

    solved task_5_1a.py

--- 5_control_structures/task_5_1a.py
+++ 5_control_structures/task_5_1a.py
@@ -13,3 +13,21 @@
 Если адрес задан неправильно, выводить сообщение:
 'Incorrect IPv4 address'
 '''
+
+ip_addr = raw_input("\nEnter IP address in dotted decimal notation: ")
+
+first_byte = int(ip_addr.split(".")[0])
+
+if len(ip_addr.split(".")) == 4:
+	if first_byte in range(0, 224):
+		print "\nunicast\n"
+	elif first_byte in range(224, 240):
+		print "\nmulticast\n"
+	elif ip_addr == "255.255.255.255":
+		print "\nlocal broadcast\n"
+	elif ip_addr == "0.0.0.0":
+		print "\nunassigned\n"
+	else:
+		print "\nunused\n"
+else:
+	print "\nIncorrect IPv4 address\n"
