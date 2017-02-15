efb8a5cfc815661e799284abc2fa19c71a25c297
pyeng <pyeng@users.noreply.github.com>
2017-02-15 11:51:54 +0200

    solved task_5_2.py

--- 5_control_structures/task_5_2.py
+++ 5_control_structures/task_5_2.py
@@ -14,3 +14,9 @@
 mac = ['aabb:cc80:7000','aabb:dd80:7340','aabb:ee80:7000','aabb:ff80:7000']
 
 mac_cisco = []
+
+for i in mac:
+	i  = ".".join(i.split(":"))
+	mac_cisco.append(i)
+
+print mac_cisco
