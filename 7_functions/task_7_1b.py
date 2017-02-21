4c175217f5096b61ae1c35a4da664248a7cdd0f9
pyeng <pyeng@users.noreply.github.com>
2017-02-21 13:11:17 +0200

    add solved task_7_1b.py

--- 7_functions/task_7_1b.py
+++ 7_functions/task_7_1b.py
@@ -1,54 +1,73 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 7.1b
 
 Сделать копию скрипта задания 7.1a.
 
 Изменить скрипт таким образом, чтобы функция возвращала не список команд, а словарь:
-    - ключи: имена интерфейсов, вида 'FastEthernet0/12'
+    - ключи: имена интерфейсов, вида "FastEthernet0/12"
     - значения: список команд, который надо выполнить на этом интерфейсе:
-                          ['switchport mode access',
-                           'switchport access vlan 10',
-                           'switchport nonegotiate',
-                           'spanning-tree portfast',
-                           'spanning-tree bpduguard enable']
+                          ["switchport mode access",
+                           "switchport access vlan 10",
+                           "switchport nonegotiate",
+                           "spanning-tree portfast",
+                           "spanning-tree bpduguard enable"]
 
 
 Проверить работу функции на примере словаря access_dict,
 с генерацией конфигурации port-security и без.
-'''
+"""
 
-def generate_access_config(access):
+import pprint
+
+def generate_access_config(access, psecurity=False):
     """
     access - словарь access-портов,
     для которых необходимо сгенерировать конфигурацию, вида:
-        { 'FastEthernet0/12':10,
-          'FastEthernet0/14':11,
-          'FastEthernet0/16':17 }
+        { "FastEthernet0/12":10,
+          "FastEthernet0/14":11,
+          "FastEthernet0/16":17 }
 
     psecurity - контролирует нужна ли настройка Port Security. По умолчанию значение False
         - если значение True, то настройка выполняется с добавлением шаблона port_security
         - если значение False, то настройка не выполняется
 
     Функция возвращает словарь:
-    - ключи: имена интерфейсов, вида 'FastEthernet0/1'
+    - ключи: имена интерфейсов, вида "FastEthernet0/1"
     - значения: список команд, который надо выполнить на этом интерфейсе
     """
 
-    access_template = ['switchport mode access',
-                       'switchport access vlan',
-                       'switchport nonegotiate',
-                       'spanning-tree portfast',
-                       'spanning-tree bpduguard enable']
+    access_template = ["switchport mode access",
+                       "switchport access vlan",
+                       "switchport nonegotiate",
+                       "spanning-tree portfast",
+                       "spanning-tree bpduguard enable"]
 
-    port_security = ['switchport port-security maximum 2',
-                     'switchport port-security violation restrict',
-                     'switchport port-security']
+    port_security = ["switchport port-security maximum 2",
+                     "switchport port-security violation restrict",
+                     "switchport port-security"]
+
+    command_dict = {}
+    
+    for intf in access:
+        command_dict[intf] = []
+        for command in access_template:
+            if command.endswith("access vlan"):
+                command_dict[intf].append("{} {}".format(command, access_dict[intf]))
+            else:
+                command_dict[intf].append(command)
+        if psecurity == True:
+            for command in port_security:
+                command_dict[intf].append(command)
+
+    return command_dict
 
 
+access_dict = { "FastEthernet0/12":10,
+                "FastEthernet0/14":11,
+                "FastEthernet0/16":17,
+                "FastEthernet0/17":150 }
 
+result = generate_access_config(access_dict, True)
 
-access_dict = { 'FastEthernet0/12':10,
-                'FastEthernet0/14':11,
-                'FastEthernet0/16':17,
-                'FastEthernet0/17':150 }
+pprint.pprint(result)
