9ee8949b0a32bc21d9ac497aeb98b3a4b645dd0e
pyeng <pyeng@users.noreply.github.com>
2017-03-09 15:30:17 +0200

    solved task_8_2a

--- 8_modules/task_8_2a.py
+++ 8_modules/task_8_2a.py
@@ -20,3 +20,16 @@
 
 from task_8_2 import parse_cdp_neighbors
 from draw_network_graph import draw_topology
+
+if __name__ == "__main__":
+
+	from sys import argv
+	from pprint import pprint
+
+	cdp_file = argv[1]
+
+	with open(cdp_file, "r") as f:
+		cdp_data = f.read()
+		result = parse_cdp_neighbors(cdp_data)
+
+	draw_topology(result)
