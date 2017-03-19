3bc9a7fbee29b41d8ff7519babbf8ed4b01465ee
pyeng <pyeng@users.noreply.github.com>
2017-03-19 17:42:51 +0200

    solved task_9_3a

--- 9_regex/task_9_3a.py
+++ 9_regex/task_9_3a.py
@@ -17,3 +17,43 @@
 
 Проверить работу функции на примере файла config_r1.txt.
 '''
+
+from sys import argv
+from re import compile
+from pprint import pprint
+
+
+def parse_cfg(file, exp):
+	#open file
+	with open(file, "r") as f:
+		# and read file
+		line = f.read()
+		#split by "!"
+		for section in (line.strip()).split("!"):
+			#leave only interface section with configured ip addr
+			if ((section.strip()).startswith("interface") and "ip addr" in section) \
+				and "no ip addr" not in section:
+
+				for i in (section.strip()).split("\n"):
+					#find interface name
+					if "interface " in i:
+						intf = i.split("interface")[1]
+						ip_addr[intf] = ()
+					#find ip addr and mask with regexp
+					if "ip addr" in i:
+						match = exp.search(i)
+						ip = match.group("ip")
+						mask = match.group("mask")
+						ip_addr[intf] = (ip, mask)
+
+	return ip_addr
+
+
+filename = argv[1]
+regexp = compile("(?P<ip>[\d+\.]+)\s+(?P<mask>[\d+\.]+)")
+
+ip_addr = {}
+
+parse_cfg(filename, regexp)
+
+pprint(ip_addr)
